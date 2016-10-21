4c3bb7831a7500897f85d3f4e36fa027eae950a8
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-21 13:17:22 +0200

    Primer comit

---
+++ test/demo02/TestSum.java
@@ -0,0 +1,19 @@
+package demo02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSum {
+
+	@Test
+	public void testSum2Number() {
+		assertEquals(6, sum(2, 4));
+	}
+	
+	
+	int sum(int a, int b) {
+		return 6;
+	}
+
+}
